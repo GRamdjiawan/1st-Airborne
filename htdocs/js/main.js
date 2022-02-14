2a46c8566a8945b15bbea453d9e55ca68e939a4b
GRamdjiawan <302325196@student.rocmondriaan.nl>
2022-02-14 17:34:52 +0100

    htdocs made
    i made a htdocs

---
+++ htdocs/js/main.js
@@ -0,0 +1,16 @@
+function drawHomePage() {
+    const homePage = document.querySelector('#home-page');
+    const backgroundHomePage = homePage.style.backgroundImage = 'url(/htdocs/img/bg/home.png)';
+}
+
+function drawAboutPage() {
+    const homePage = document.querySelector('#about-us');
+    const backgroundHomePage = homePage.style.backgroundImage = 'url(/htdocs/img/bg/about-us-amercian.jpg)';
+}
+
+
+
+
+drawHomePage()
+
+drawAboutPage()
