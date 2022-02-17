9bbf8be2ac96153a7f561365c5d9384ba18bbc15
GRamdjiawan <302325196@student.rocmondriaan.nl>
2022-02-17 21:21:05 +0100

    background images fixt
    background images fixt

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,11 +1,11 @@
 function drawHomePage() {
     const homePage = document.querySelector('#home-page');
-    const backgroundHomePage = homePage.style.backgroundImage = 'url(/htdocs/img/bg/home.png)';
+    const backgroundHomePage = homePage.style.backgroundImage = 'url(img/bg/home.png)';
 }
 
 function drawAboutPage() {
     const homePage = document.querySelector('#about-us');
-    const backgroundHomePage = homePage.style.backgroundImage = 'url(/htdocs/img/bg/about-us.jpg)';
+    const backgroundHomePage = homePage.style.backgroundImage = 'url(img/bg/about-us.jpg)';
 }
 
 
