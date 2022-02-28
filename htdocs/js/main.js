946f4385c38adf886d1e585c35b400a4d2a2e121
GRamdjiawan <302325196@student.rocmondriaan.nl>
2022-02-28 18:49:27 +0100

    clip page added
    i added the clip page to the website and made it responsive

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -8,6 +8,33 @@ function drawAboutPage() {
     const backgroundHomePage = homePage.style.backgroundImage = 'url(img/bg/about-us.jpg)';
 }
 
+function drawClipPage() {
+    const clipPage = document.querySelector('#clips');
+    const backgroundClipPage = clipPage.style.backgroundImage = 'url(img/bg/clips.jpg)';
+}
+
+function playClip() {
+    const clips = document.querySelectorAll('.clips')
+
+    clips.forEach((e) => {
+        // when you hover over a clip it will start playing
+        e.addEventListener('mouseenter', () => {
+            e.play();
+
+        });
+
+        //  when you hover over a clip it will stop playing and reset the video
+        e.addEventListener('mouseout', () => {
+            e.pause();
+            e.currentTime = 0;
+
+        });
+
+    })
+}
+
 
 drawHomePage();
 drawAboutPage();
+drawClipPage()
+playClip();
